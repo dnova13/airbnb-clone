315331c85baaf7bf750250b8e410522cfc050842
dnova13 <dnova13@gmail.com>
2021-09-16 19:15:12 +0900

    6.1 Room Admin Panel part 2

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,6 +15,23 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
 
+    fieldsets = (
+        (
+            "Basic Info",  # 제목
+            {"fields": ("name", "description", "country", "address", "price")},  # 목록
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
     list_display = (
         "name",
         "country",
@@ -27,11 +44,26 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
     )
 
-    list_filter = ("instant_book", "city", "country")
+    # 포린키 호스트에서 다른거 보여주고 싶다면
+    # host__superhost  이렇게 적으면됨
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
 
     search_fields = ("=city", "^host__username")
+
+    # horizon 패널은 다대다 관계만 가능.
+    filter_horizontal = ("amenities", "facilities", "house_rules")
 
 
 @admin.register(models.Photo)
