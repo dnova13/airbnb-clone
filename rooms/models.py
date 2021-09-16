64433b631d529bd522b110e36fceb547b78ca8d9
dnova13 <dnova13@gmail.com>
2021-09-16 13:56:29 +0900

    4.5 Meta Class and Photos Model

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
 
 
 class AbstractItem(core_models.TimeStampedModel):
@@ -11,31 +10,27 @@ class AbstractItem(core_models.TimeStampedModel):
     name = models.CharField(max_length=80)
 
     class Meta:
+        # 추상 모델 허용.
         abstract = True
 
     def __str__(self):
         return self.name
 
 
-# 하나의 방에서는
-# 방 타입뿐만 아니라
-# 방에서 제공하는 소모품(Amenity), 시설(Facility) 등이 잇으므로
-# 이 방 모델 클래스에 하위에 속하는 자식 클래스들을 적음.
-# 이 자식 클라스에 각각의 아이템들은 AbstractItem 으로 확장받아 고드 중복을 없앤다.
-# 예로 이름 무조건 필요하니
-# `name = models.CharField(max_length=80)` 이런 중복된 코드 적는걸 막는다.
 class RoomType(AbstractItem):
 
     """RoomType Model Definition"""
 
-    pass
+    class Meta:
+        verbose_name = "Room Type"
 
 
 class Amenity(AbstractItem):
 
     """Amenity Model Definition"""
 
-    pass
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
@@ -44,12 +39,33 @@ class Facility(AbstractItem):
 
     pass
 
+    class Meta:
+        verbose_name_plural = "Facilities"
+
 
 class HouseRule(AbstractItem):
 
     """HouseRule Model Definition"""
 
-    pass
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """Photo Model Definition"""
+
+    # 사진에 대한 설명
+    caption = models.CharField(max_length=80)
+
+    #  이미지 파일
+    file = models.ImageField()
+
+    # 방이 삭제 되면 거기에 종속된 포토도 삭제
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -69,11 +85,11 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity)
-    facilities = models.ManyToManyField(Facility)
-    house_rules = models.ManyToManyField(HouseRule)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
