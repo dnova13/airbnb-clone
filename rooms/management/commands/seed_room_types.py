a2d63a4e49f415a619c361082cbfcd034f409db7
dnova13 <dnova13@gmail.com>
2021-09-19 13:17:33 +0900

    9.3 seed_rooms part 1

---
+++ rooms/management/commands/seed_room_types.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from rooms.models import RoomType
+
+
+class Command(BaseCommand):
+
+    help = "This command creates facilities"
+
+    def handle(self, *args, **options):
+        roomType = [
+            "Entire place",
+            "Private room",
+            "Hotel room",
+            "Shared room",
+        ]
+        for rt in roomType:
+            RoomType.objects.create(name=rt)
+
+        self.stdout.write(self.style.SUCCESS(f"{len(roomType)} facilities created!"))
