a1079ccc542aaeaf9c9c6b0b288b2b5617a68059
dnova13 <dnova13@gmail.com>
2021-09-20 20:01:08 +0900

    11.1 HomeView :  Pagination with Limit and Offset

--- rooms/views.py
+++ rooms/views.py
@@ -3,5 +3,11 @@ from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    # page 키로 값을 받고, 아무것도 받지않는다면 디폴트로 1을 설정
+    page = int(request.GET.get("page", 1))  # int 로 변화
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+
+    return render(request, "rooms/home.html", {"rooms": all_rooms})
