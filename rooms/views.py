1ff9081a286cc756931fbe85de057a6a6d9d0370
dnova13 <dnova13@gmail.com>
2021-09-21 19:58:27 +0900

    11.5 Django Paginator : orphans(고아)

--- rooms/views.py
+++ rooms/views.py
@@ -5,11 +5,16 @@ from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
 
     # (obj, perpage), 10은 per_page 페이지당 보여줄 개수
-    paginator = Paginator(room_list, 10)  
-    rooms = paginator.get_page(page)
-
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    # orphans : 마지막 페이지에 보여줄 개수 상한 지정.
+    # 예로
+    # 1. 마지막 페이지 20페이지가 방개수 5개 이하면
+    #    19페이지로 그 5개의 방이 넘어가면서 19페이지에 15개의 방 보임
+    # 2. 마지막 페이지가 5개 초과할 경우
+    #    마지막 페이지에는 6개부터 방을 보여줌.
+    paginator = Paginator(room_list, 10, orphans=5)
+    rooms = paginator.page(int(page))
+    return render(request, "rooms/home.html", {"page": rooms})
