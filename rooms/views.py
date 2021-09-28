5b75bc5a32aae7c22c0e234911611a5b3075539b
dnova13 <dnova13@gmail.com>
2021-09-29 04:40:32 +0900

    11.8 Class Based Views part 2 : context_object_name, get_context_data()

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.utils import timezone
 from . import models
 
 
@@ -8,7 +9,17 @@ class HomeView(ListView):
     """HomeView Definition"""
 
     model = models.Room
+
     # 페이지에서 보일 목록의 개수
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
+
+    # object 이름을 rooms 로 변경
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
