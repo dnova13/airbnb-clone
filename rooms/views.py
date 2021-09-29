0d875203d1ca8b0e33c5b5d2e1ac5409f418b125
dnova13 <dnova13@gmail.com>
2021-09-30 02:52:12 +0900

    12.2 room_detail FBV finished

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from . import models
 
@@ -27,5 +28,13 @@ class HomeView(ListView):
 
 
 def room_detail(request, pk):
-    print(pk)
-    return render(request, "rooms/detail.html")
+    try:
+        # db에서 해당 pk 즉 아이디에서의 방정보를 가져옴.
+        room = models.Room.objects.get(pk=pk)
+
+        # db 에서 방하나의 정보를 랜더함.
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+
+        # 이전 url 장고 기법 활용하여 "/" 로 리다렉
+        return redirect(reverse("core:home"))
