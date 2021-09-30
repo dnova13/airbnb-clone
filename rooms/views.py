310680a2768310355d7abf08c51d977bfe5850a4
dnova13 <dnova13@gmail.com>
2021-10-01 08:09:25 +0900

    13.3 Amenities and Facilities Form

--- rooms/views.py
+++ rooms/views.py
@@ -43,24 +43,43 @@ def search(request):
     city = request.GET.get("city", "Anywhere")
     country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room_type", 0))
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    s_amenities = request.GET.get("amenities")
+    s_facilities = request.GET.get("facilities")
+    print(s_amenities, s_facilities)
 
     # capitalize 앞문자만 대문자로 만들고 나머지 소문자로.
     city = str.capitalize(city)
-
-    # db 에서 룸타입 다 가져옴.
-    room_types = models.RoomType.objects.all()
 
     # 서치 폼에서 검색한 조건들.
     form = {
         "city": city,
         "s_room_type": room_type,  # 선택한 룸 타입
         "s_country": country,  # 선택한 나라
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
     }
+
+    # db 에서 룸타입 다 가져옴.
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
 
     # 각 검색 태그에 들어갈 선택 조건들
     choices = {
         "countries": countries,
         "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
     }
 
     return render(request, "rooms/search.html", {**form, **choices})
