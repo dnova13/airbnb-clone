93fe16ed52907e3153e0d368c5e9fa9d0e0b01e1
dnova13 <dnova13@gmail.com>
2021-10-03 15:27:21 +0900

    15.2. ModelForms

--- users/forms.py
+++ users/forms.py
@@ -23,11 +23,11 @@ class LoginForm(forms.Form):
             self.add_error("email", forms.ValidationError("User does not exist"))
 
 
-class SignUpForm(forms.Form):
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
 
-    first_name = forms.CharField(max_length=80)
-    last_name = forms.CharField(max_length=80)
-    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
     password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
@@ -43,24 +43,19 @@ class SignUpForm(forms.Form):
     def clean_password1(self):
         password = self.cleaned_data.get("password")
         password1 = self.cleaned_data.get("password1")
-
         if password != password1:
             raise forms.ValidationError("Password confirmation does not match")
         else:
             return password
 
-    def save(self):
-        first_name = self.cleaned_data.get("first_name")
-        last_name = self.cleaned_data.get("last_name")
+    def save(self, *args, **kwargs):
+
+        # commit=False 로 작성하여 db 에 저장 못하게 막음.
+        user = super().save(commit=False)
+
+        # 모델폼에 포함되지 않은 다른 필요한 양식들 적어서 db에 저장.
         email = self.cleaned_data.get("email")
         password = self.cleaned_data.get("password")
-
-        # 패스워드 암호화 저장하기 위해서 장고 지원하는 메소드 create_user 이용
-        # creat_urser(username, email, password) 형식으로 지원.
-        user = models.User.objects.create_user(email, email, password)
-
-        user.first_name = first_name
-        user.last_name = last_name
-
-        # 그 다음 만든 user를 가져와서 새로 등록할 파라미터를 저장함.
+        user.username = email
+        user.set_password(password)
         user.save()
