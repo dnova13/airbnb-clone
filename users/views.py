eb7971e302565aac9f87ef39935d23ab44d2beb8
dnova13 <dnova13@gmail.com>
2021-10-03 10:56:06 +0900

    14.5 LoginView eaiser way by FormView

--- users/views.py
+++ users/views.py
@@ -1,17 +1,41 @@
 from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
 
-class LoginView(View):
-    def get(self, request):
+class LoginView(FormView):
+
+    # 응답받을 templates html 지정.
+    template_name = "users/login.html"
+
+    # 어떤 용도로 쓸지 클래스 지정.
+    form_class = forms.LoginForm
+
+    # 해당 요청 예로 로그인 성공시 리다렉 지정.
+    # reverse를 통해 ("core:home") 이 적용 안됨
+    # reverse_lazy 이용
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+
+        if user is not None:
+            login(self.request, user)
+
+        return super().form_valid(form)
+
+    """ def get(self, request):
 
         form = forms.LoginForm()
 
-        return render(request, "users/login.html", {"form": form})
+        return render(request, "users/login.html", {"form": form}) """
 
-    def post(self, request):
+    """ def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data.get("email")
@@ -23,7 +47,7 @@ class LoginView(View):
                 #
                 return redirect(reverse("core:home"))
 
-        return render(request, "users/login.html", {"form": form})
+        return render(request, "users/login.html", {"form": form}) """
 
 
 def log_out(request):
