2d4bbbdd0e872fb451f9d4bc04e10f2133592f0b
dnova13 <dnova13@gmail.com>
2021-10-19 09:13:14 +0900

    23.0 Update Room View

--- rooms/urls.py
+++ rooms/urls.py
@@ -6,5 +6,6 @@ app_name = "rooms"
 # as_view() 로 RoomDetail view 로 전환
 urlpatterns = [
     path("<int:pk>", views.RoomDetail.as_view(), name="detail"),
+    path("<int:pk>/edit/", views.EditRoomView.as_view(), name="edit"),
     path("search/", views.SearchView.as_view(), name="search"),
 ]
