4bebdbf19bfb8e20e4a9573a5f6d3a4c2114b5cf
dnova13 <dnova13@gmail.com>
2021-11-01 21:12:48 +0900

    25.4 Saving Fav(favorite) Rooms

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, reverse
+from rooms import models as room_models
+from . import models
 
-# Create your views here.
+
+# 선호하는 방 추가
+def save_room(request, room_pk):
+    room = room_models.Room.objects.get_or_none(pk=room_pk)
+
+    if room is not None:
+        # get_or_create : 해당 데이터가 있으면 get, 없으면 데이터 생성함.
+        the_list, _ = models.List.objects.get_or_create(
+            user=request.user, name="My Favourites Houses"
+        )
+        the_list.rooms.add(room)
+
+    return redirect(reverse("rooms:detail", kwargs={"pk": room_pk}))
