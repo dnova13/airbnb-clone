e407878cc5fdae153b99d26328c2b5c5e9243274
dnova13 <dnova13@gmail.com>
2021-11-10 21:38:29 +0900

    26.5 container_commands deploy 5

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -3,8 +3,7 @@
 source /var/app/venv/*/bin/activate
 cd /var/app/staging
 
-python manage.py compilemessages
-# python manage.py makemigrationspython manage.py compilemessages
 python manage.py migrate
+# python manage.py makemigrationspython manage.py compilemessages
 # python manage.py createfirstsuperuser
 # python manage.py collectstatic --noinput
