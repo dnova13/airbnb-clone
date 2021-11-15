7f43e946e54d29fcf664a793b1983ffb1ecc52e2
dnova13 <dnova13@gmail.com>
2021-11-15 15:15:25 +0900

    test daphne

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -4,3 +4,4 @@ source /var/app/venv/*/bin/activate
 cd /var/app/staging
 
 python manage.py migrate
+# daphne -b 0.0.0.0 -p 8452 config.asgi:application
--- config/asgi.py
+++ config/asgi.py
@@ -1,7 +1,18 @@
+# import os
+
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# application = get_asgi_application()
+
+
 import os
+import django
+from channels.routing import get_default_application
 
-from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
 
-application = get_asgi_application()
+application = get_default_application()
